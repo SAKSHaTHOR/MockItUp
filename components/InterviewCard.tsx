c43ea9fc1686f854c4ff41e405c07941de7d3843
SAKSHaTHOR <sakshatwalishetti555@gmail.com>
2025-04-11 11:16:40 +0530

    implement session page

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,7 +5,7 @@ import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
 
-const InterviewCard = ({ interviewId,userId,role,type,techstack,createdAt }: InterviewCardProps) =>{
+const InterviewCard = ({ id,userId,role,type,techstack,createdAt }: InterviewCardProps) =>{
 
     const feedback = null as Feedback | null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
@@ -49,8 +49,8 @@ const InterviewCard = ({ interviewId,userId,role,type,techstack,createdAt }: Int
 
                     <Button className="btn-primary">
                         <Link href={feedback
-                            ? `/interview/${interviewId}/feedback`
-                            : `/interview/${interviewId}`
+                            ? `/interview/${id}/feedback`
+                            : `/interview/${id}`
                         }>
 
                             {feedback ? 'Check Feedback' : 'View Interview'}
